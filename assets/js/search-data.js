5c152a5d4c89bb123ecfaf4743ff5c63cc62dd65
vineblack <101965129+vineblack@users.noreply.github.com>
2026-02-15 18:46:58 +0000

    Deploying to gh-pages from @ vineblack/vineblack.github.io@9b79e46b73e60522b43e0555225284e974120d8b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,7 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6C%6C%73%74%65%6C%6C%61%36%37%34@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
