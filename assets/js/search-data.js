52870ab11ab4913758276e63959eaf49994d55c9
vineblack <101965129+vineblack@users.noreply.github.com>
2026-07-22 10:59:31 +0000

    Deploying to gh-pages from @ vineblack/vineblack.github.io@c1072d6e1e04f2e457c4caeb3fb13265b5667892 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Youngeun Lee — Narrative 3D Environment Artist",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
